406a71a392a166a5d479c1b7507a15fdc1cf52c7
Bamer Balazs <balazs@bamer.hu>
2022-04-10 14:14:48 +0200

    Fixed precision problem in eikonal.cpp output.

--- eikonal.cpp
+++ eikonal.cpp
@@ -1,4 +1,6 @@
 #include  <boost/program_options.hpp>
+#include <iostream>
+#include <iomanip>
 #include "nr3.h"
 #include "stepper.h"
 #include "stepperdopr853.h"
@@ -9,7 +11,7 @@
 #include "StepperDormandPrice853.h"
 #include "RungeKuttaRayBending.h"
 
-//clang++ -Inumrec -I/usr/include/eigen3 -I../repos/eigen-initializer_list/src -DEIGEN_MATRIX_PLUGIN=\"Matrix_initializer_list.h\" -DEIGEN_ARRAY_PLUGIN=\"Array_initializer_list.h\" -std=c++17 eikonal.cpp mathUtil.cpp -o eikonal -ggdb -lboost_program_options
+//clang++ -Inumrec -I/usr/include/eigen3 -I../repos/eigen-initializer_list/src -DEIGEN_MATRIX_PLUGIN=\"Matrix_initializer_list.h\" -DEIGEN_ARRAY_PLUGIN=\"Array_initializer_list.h\" -std=c++17 eikonal.cpp mathUtil.cpp -o eikonal -ggdb -lboost_program_options -lgsl
 
 
 // These two are for conventional asphalt
@@ -80,14 +82,14 @@ public:
   }
 };
 
-void comp(double aDir, double aDist, double aHeight, double aStep1, double aStepMin, double aTempAmb, double aTempDiff, double aTolAbs, double aTolRel, double aTarget) {
-  constexpr uint32_t cNvar = 6u;
+void comp(double aDir, double aDist, double aHeight, double aStep1, double aStepMin, double aTempAmb, double aTempDiff, double aTolAbs, double aTolRel, double aTarget, uint32_t aSamples) {
+/*  constexpr uint32_t cNvar = 6u;
   VecDoub yStart(cNvar);
   yStart[0] = 0.0;
   yStart[1] = 0.0;
-  yStart[2] = aHeight;
+  yStart[2] = aHeight;*/
   auto u = refract(aHeight, aTempAmb, aTempDiff) / Eik::cgC;
-  yStart[3] = u * std::cos(aDir / 180.0 * 3.1415926539);
+/*  yStart[3] = u * std::cos(aDir / 180.0 * 3.1415926539);
   yStart[4] = 0.0;
   yStart[5] = u * std::sin(aDir / 180.0 * 3.1415926539);
   Output out(100);
@@ -120,17 +122,17 @@ void comp(double aDir, double aDist, double aHeight, double aStep1, double aStep
   aTarget /= 2;
   result = ode2.solve(yStart2, judge, 0.0001);
   std::cout << "xnrm2=[0, " << result[0] << "];\n";
-  std::cout << "znrm2=[" << aHeight << ", " << result[2] << "];\n";
+  std::cout << "znrm2=[" << aHeight << ", " << result[2] << "];\n";*/
 
   Eikonal eikonal(aTempAmb, aTempDiff);
-  RungeKuttaRayBending rk(aDist, aTolAbs, aTolRel, aStep1, aStepMin, eikonal);
+/*  RungeKuttaRayBending rk(aDist, aTolAbs, aTolRel, aStep1, aStepMin, eikonal);
   Vertex start(0.0f, 0.0f, (float)(aHeight));
   Vector dir((float)(std::cos(aDir / 180.0 * 3.1415926539)), 0.0f, (float)(std::sin(aDir / 180.0 * 3.1415926539)));
   auto final = rk.solve4x(start, dir, aTarget * 0.75);
   std::cout << "xnrrb=[0, " << final(0) << "];\n";
-  std::cout << "znrrb=[" << aHeight << ", " << final(2) << "];\n";
+  std::cout << "znrrb=[" << aHeight << ", " << final(2) << "];\n";*/
 
-  OdeSolverGsl<Eikonal> gsl(0.0, aDist, 1e-6, 1e-6, 1e-6, eikonal);
+  OdeSolverGsl<Eikonal> gsl(0.0, aDist, aTolAbs, aTolRel, aStep1, eikonal);
   typename Eikonal::Variables yStart3;
   yStart3[0] = 0.0;
   yStart3[1] = aHeight;
@@ -139,17 +141,17 @@ void comp(double aDir, double aDist, double aHeight, double aStep1, double aStep
   yStart3[4] = u * std::sin(aDir / 180.0 * 3.1415926539);
   yStart3[5] = 0.0;
   std::vector<typename Eikonal::Variables> stuff;
-  for(double t = 0.0; t < aDist; t += aDist / 100.0) {
+  for(double t = 0.0; t < aTarget; t += aTarget / aSamples) {
     auto [tt,y] = gsl.solve(yStart3, [t](typename Eikonal::Variables const &aY){ return aY[0] > t; });
     stuff.push_back(y);
   }
   std::cout << "xgls=[";
   for (int i = 0; i < stuff.size(); ++i) {
-    std::cout << stuff[i][0] << (i < stuff.size() - 1 ? ", " : "];\n");
+    std::cout << std::setprecision(10) << stuff[i][0] << (i < stuff.size() - 1 ? ", " : "];\n");
   }
   std::cout << "zgls=[";
   for (int i = 0; i < stuff.size(); ++i) {
-    std::cout << stuff[i][1] << (i < stuff.size() - 1 ? ", " : "];\n");
+    std::cout << std::setprecision(10) << stuff[i][1] << (i < stuff.size() - 1 ? ", " : "];\n");
   }
   std::cout << "\n";
 }
@@ -158,16 +160,17 @@ int main(int aArgc, char **aArgv) {
   boost::program_options::options_description desc("Usage:");
   desc.add_options()
                     ("dir", boost::program_options::value<double>(), "start direction (degrees), neg is down, 0 horizontal [0]")
-                    ("dist", boost::program_options::value<double>(), "horizontal distance to track [2000]")
+                    ("dist", boost::program_options::value<double>(), "distance along the ray to track [2000]")
                     ("height", boost::program_options::value<double>(), "start height (m) [1]")
                     ("help", "produce this message")
-                    ("step1", boost::program_options::value<double>(), "initial step size [0.01]")
+                    ("samples", boost::program_options::value<uint32_t>(), "number of samples on ray [100]")
+                    ("step1", boost::program_options::value<double>(), "initial step size [1e-6]")
                     ("stepmin", boost::program_options::value<double>(), "minimal step size [0.0]")
+                    ("target", boost::program_options::value<double>(), "horizontal distance to travel [1000]")
                     ("tempamb", boost::program_options::value<double>(), "ambient temperature (Celsius) [20]")
                     ("tempdiff", boost::program_options::value<double>(), "temperature rise next to asphalt compared to ambient (Celsius) [6.37]")
-                    ("tolabs", boost::program_options::value<double>(), "absolute tolerance [1e-3]")
-                    ("tolrel", boost::program_options::value<double>(), "relative tolerance [1e-3]")
-                    ("xyz", boost::program_options::value<double>(), "custom [0]");
+                    ("tolabs", boost::program_options::value<double>(), "absolute tolerance [1e-6]")
+                    ("tolrel", boost::program_options::value<double>(), "relative tolerance [1e-6]");
   boost::program_options::variables_map varMap;
   boost::program_options::store(boost::program_options::parse_command_line(aArgc, aArgv, desc), varMap);
   boost::program_options::notify(varMap);
@@ -178,14 +181,15 @@ int main(int aArgc, char **aArgv) {
     double dir = (varMap.count("dir") ? varMap["dir"].as<double>() : 0.0);
     double dist = (varMap.count("dist") ? varMap["dist"].as<double>() : 2000.0);
     double height = (varMap.count("height") ? varMap["height"].as<double>() : 1.0);
-    double step1 = (varMap.count("step1") ? varMap["step1"].as<double>() : 0.01);
+    uint32_t samples = (varMap.count("samples") ? varMap["samples"].as<uint32_t>() : 100);
+    double step1 = (varMap.count("step1") ? varMap["step1"].as<double>() : 1e-6);
     double stepMin = (varMap.count("stepmin") ? varMap["stepmin"].as<double>() : 0.0);
+    double target = (varMap.count("target") ? varMap["target"].as<double>() : 1000.0);
     double tempAmb = (varMap.count("tempamb") ? varMap["tempamb"].as<double>() : 20);
     double tempDiff = (varMap.count("tempdiff") ? varMap["tempdiff"].as<double>() : 6.37);
-    double tolAbs = (varMap.count("tolabs") ? varMap["tolabs"].as<double>() : 0.001);
-    double tolRel = (varMap.count("tolrel") ? varMap["tolrel"].as<double>() : 0.001);
-    double target = (varMap.count("xyz") ? varMap["xyz"].as<double>() : 0.0);
-    comp(dir, dist, height, step1, stepMin, tempAmb, tempDiff, tolAbs, tolRel, target);
+    double tolAbs = (varMap.count("tolabs") ? varMap["tolabs"].as<double>() : 1e-6);
+    double tolRel = (varMap.count("tolrel") ? varMap["tolrel"].as<double>() : 1e-6);
+    comp(dir, dist, height, step1, stepMin, tempAmb, tempDiff, tolAbs, tolRel, target, samples);
   }
   return 0;
 }
